3eb629445e9deacefe10764d46c5a4f106b14459
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2018-11-10 16:42:31 +0900

    現在時刻をデフォルト値に

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,8 @@ module RoutineWork
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
   end
 end
