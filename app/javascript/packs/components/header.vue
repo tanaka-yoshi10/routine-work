c77c1c3d953f79a2e71e069efe0ec77f0d045e42
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2018-11-18 05:45:34 +0900

    rruleを変換してみる

--- app/javascript/packs/components/header.vue
+++ app/javascript/packs/components/header.vue
@@ -2,11 +2,12 @@
   <div class="form-group string optional routine_rrule form-group-valid">
     <label for="routine_rrule" class="form-control-label string optional">Rrule</label>
     <input v-model='rule' type="text" name="routine[rrule]" id="routine_rrule" class="form-control is-valid string optional" data-com.agilebits.onepassword.user-edited="yes">
-    <p>{{ rule }}</p>
+    <p>{{ convert() }}</p>
   </div>
 </template>
 
 <script>
+import { RRule, RRuleSet, rrulestr } from 'rrule'
 export default {
   data: function () {
     return {
@@ -16,7 +17,15 @@ export default {
   mounted: function() {
     this.rule = this.value
   },
-  props: ['value']
+  props: ['value'],
+  methods: {
+    convert: function() {
+      if (this.rule) {
+        const rrule = rrulestr(this.rule)
+        return rrule.toText()
+      }
+    }
+  }
 }
 </script>
 
